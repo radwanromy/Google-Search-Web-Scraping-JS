8357f86e8698f5b888e1fc8c1c673e2d03d2fd06
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2022-09-09 21:45:11 +0600

    Commit Elobyte 003

--- wsjson/server.js
+++ wsjson/server.js
@@ -7,73 +7,79 @@ async function scrape(url) {
     const inputFiled = await page.$(
         "body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input"
     );
+    
 
     await inputFiled.type("Elobyte BD");
     await page.keyboard.press("Enter");
-    await page.waitForSelector(".MjjYud");
-    const results = await page.evaluate(() => {
 
+    await page.waitForSelector(".v7W49e");
+    
+    const resultsmain = await page.evaluate(async () => {
 
+        // const descriptionmain= await document.querySelector(".MjjYud").innerText;
 
-        const searchResults = document.querySelectorAll(".MjjYud");
+        // const x = document.querySelectorAll('.gLFyf').value;
+
+        const searchResultsMain = document.querySelectorAll(".MjjYud");
+        
         const countAll = document.querySelectorAll('.MjjYud').length;
         const countAllvideos = document.querySelectorAll('.uOId3b').length;
+        const Websites = document.querySelector(".yuRUbf cite").innerText;
         console.log("The number of results", countAll);
-        // const { gotScraping } = require('got-scraping');
-
-        // gotScraping
-        //     .get('https://www.facebook.com/')
-        //     .then( ({ body }) => console.log(body))
-
-
-        //     $.getJSON( 'http://graph.facebook.com/?id=' + $URL, function( fbdata ) {
-        //     $('#facebook-count').text( 'The URL has ' + ReplaceNumberWithCommas(fbdata.shares) + ' shares count on Facebook')
-        // });
-
-        //    const countfbsearchResults = fbdata.length;
-
+        
         const temp = [];
 
-        searchResults.forEach(searchItem => {
-            // let myString = searchItem.querySelectorAll(".MjjYud a").href;
-            // let mySubString = "/facebook.com/";
-            // let countfb = myString.split(mySubString).length - 1;
-            // console.log(countfb);
-
-            // var temp = searchItem.querySelectorAll(".MjjYud a").href;
-            // var fbcount = (temp.match("facebook") || []).length;
-            // console.log(fbcount);
-            //     const nodeList =  searchItem.querySelector(".MjjYud a").herf;  // get only <a> elements that have the classname 'survey-processed' and whose 'href' attribute contains the phrase "/media-release"
-            //    var fbcount = (nodeList.match("facebook") || []).length;
-            //     console.log(fbcount);
-            // const fbcount=0;
-            // nodeList?forEach((nodeList)=>{
-            //     if(nodeList.hasOwnProperty('facebook'))
-            //       fbcount++;
-            //   });
-
-            let item = {
-
-
-                The_number_of_results: countAll,
-                // fb:countfbsearchResults,
-                // facebook:arr, //
-                // fb: fbcount,
-                // facebook:nodeList.length,
-                The_number_of_results_that_are_video_links: countAllvideos,
-                Title: searchItem.querySelector(".MjjYud h3").innerHTML,
-                URL: searchItem.querySelector(".MjjYud a").href,
-
-
+        searchResultsMain.forEach(searchItems => {
+            let info = {
+                pageOneResultCount: countAll,
+                pageOneVideoResultCount: countAllvideos,
+                Website: Websites,
 
             };
 
-
-            temp.push(item);
+            
+           
+            temp.push(info);
         });
+        
         return temp;
     });
-    require("fs").writeFile("result.json", JSON.stringify(results), () => { });
+    require("fs").writeFile("database.json", JSON.stringify(resultsmain), () => { });
+
+
+    await page.waitForSelector(".MjjYud");
+    
+    const results = await page.evaluate(async () => {
+
+        const description= await document.querySelector(".MjjYud").innerText;
+        const searchResults = document.querySelectorAll(".MjjYud");
+        const countAll = document.querySelectorAll('.MjjYud').length;
+        const countAllvideos = document.querySelectorAll('.uOId3b').length;
+        const cites = document.querySelector(".yuRUbf cite").innerText;
+        console.log("The number of results", countAll);
+        
+        const temp = [];
+        searchResults.forEach(searchItem => {
+            let item = {
+                // pageOneResultCount: countAll,
+                // pageOneVideoResultCount: countAllvideos,
+                Website:cites,
+                Title: searchItem.querySelector(".MjjYud h3").innerHTML,
+                URL: searchItem.querySelector(".MjjYud a").href,
+                Description:description,
+
+            };
+
+            
+           
+            temp.push(item);
+        });
+        
+        return temp;
+    });
+    require("fs").writeFile("Webpages.json", JSON.stringify(results), () => { });
+
+
     browser.close();
 }
 
